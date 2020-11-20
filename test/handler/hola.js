29bc18d59cb620f63fa09372e11bd3e4731cdbb5
kaiser <cesar.aguirre@naranja.com>
2020-11-20 11:20:21 -0300

    delete test

--- test/handler/hola.js
+++
@@ -1,9 +0,0 @@
-const { ZZZ } = require('ebased/handler');
-const inputMode = require('ebased/handler/input/'+{YOUR_METHOD});
-const outputMode = require('ebased/handler/output/'+{YOUR_METHOD});
-
-const { domain } = require('../domain/hola');
-
-module.exports.handler = async (events, context) => {
-  return ZZZ({ events, context }, inputMode, domain, outputMode);
-};
